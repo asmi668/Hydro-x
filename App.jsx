ed388645fe54b251806c44ed0058f95ecff9bdd2
asmi668 <banikasmita10@gmail.com>
2024-09-18 23:36:28 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div class =" text-pink-300">hi</div>
+    </>
+  )
+}
+
+export default App
